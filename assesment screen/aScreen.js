13dcbc3a6e600c63282062b95b138cf95b831b8b
Anant Dev Dubey <anantdevdubey@gmail.com>
2017-12-25 14:30:23 +0530

    added radio buttons and put async call inside do.ready

--- assesment screen/aScreen.js
+++ assesment screen/aScreen.js
@@ -1,87 +1,86 @@
-var qList=[];
+var qList = [];
 var totalQuestions;
 
-document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
-function theDomHasLoaded(){
-$.ajax({
-  url: 'https://raw.githubusercontent.com/eadc-p/eadc-p.github.io/master/IntermediateMathematics.json',
-  dataType: 'text',
-}).done(successFunction);
-}
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://raw.githubusercontent.com/eadc-p/eadc-p.github.io/master/IntermediateMathematics.json',
+        dataType: 'text',
+    }).done(successFunction);
+});
 
 function successFunction(data) {
     qList = JSON.parse(data);
-    console.log(qList[0], qList.length/4);
-    totalQuestions = qList.length/4;
-  }
-
-window.addEventListener("load", pageFullyLoaded, false);
-function pageFullyLoaded(e) {
-    // do something again
+    console.log(qList[0], qList.length / 4);
+    totalQuestions = qList.length / 4;
     var qReference = 0;
     $('.questionList').html('');
-    for(let i=0; i<totalQuestions; i++){
-    $('.questionList').append('<a href="#" class="btn qLButton">'+ (i+1) +'</a>');
+    for (let i = 0; i < totalQuestions; i++) {
+        $('.questionList').append('<a href="#" class="btn qLButton">' + (i + 1) + '</a>');
     }
     $('.totalNo').text(totalQuestions);
     console.log(totalQuestions, qList);
     doTheThing(qReference);
 
-    $('.next').on('click', function(){
+    $('.next').on('click', function() {
         qReference++;
-        console.log(qReference+1);
+        console.log(qReference + 1);
         doTheThing(qReference);
     });
 
 
-    $('.back').on('click', function(){
+    $('.back').on('click', function() {
         qReference--;
         console.log(qReference + 1);
         doTheThing(qReference);
-        if(qReference===0){
-          $('.back').addClass('disabled');
+        if (qReference === 0) {
+            $('.back').addClass('disabled');
         }
     });
 
-    for(let i=1;i<=totalQuestions;i++){
-      $('.qLButton:nth-child(' + i + ')').on('click', function(){
-        qReference=doTheThing(i-1);
-      });
+    for (let i = 1; i <= totalQuestions; i++) {
+        $('.qLButton:nth-child(' + i + ')').on('click', function() {
+            qReference = doTheThing(i - 1);
+        });
+    }
+}
+
+function doTheThing(ref) {
+    let num = ref + 1;
+    let ques = ref * 4 + Math.floor(4 * Math.random());
+    $('.qLButton').removeClass('active');
+    $('.qLButton:nth-child(' + num + ')').addClass('active');
+    $('.questionNumber').text(num);
+    $('.numQuestion').text(num + '.Question');
+    $('.question').text(qList[ques].Question);
+    if (qList[ques]['Question Image']) {
+        $('.qImage').attr('src', qList[ques]['Question Image']);
+        $('.qImage').css('display', 'block');
+        console.log(qList[ques]['Question Image']);
+    } else {
+        $('.qImage').css('display', 'none');
+    }
+    /*
+      $('.options:nth-child(1)').text(qList[ques]['Option a (Correct)']);
+      $('.options:nth-child(2)').text(qList[ques]['Option b']);
+      $('.options:nth-child(3)').text(qList[ques]['Option c']);
+      $('.options:nth-child(4)').text(qList[ques]['Option d']);
+      */
+    $('[for=optiona]').text(qList[ques]['Option a (Correct)']);
+    $('[for=optionb]').text(qList[ques]['Option b']);
+    $('[for=optionc]').text(qList[ques]['Option c']);
+    $('[for=optiond]').text(qList[ques]['Option d']);
+    if (ref === 0) {
+        $('.back').addClass('disabled');
+    } else {
+        $('.back').removeClass('disabled');
     }
 
-  }
-
-
-function doTheThing(ref){
-  let num= ref+1;
-  let ques = ref *4 + Math.floor(4*Math.random());
-  $('.qLButton').removeClass('active');
-  $('.qLButton:nth-child(' + num + ')').addClass('active');
-  $('.questionNumber').text(num);
-  $('.numQuestion').text(num + '.Question');
-  $('.question').text(qList[ques].Question);
-  if(qList[ques]['Question Image']){
-    $('.qImage').attr('src', qList[ques]['Question Image'] );
-    $('.qImage').css('display', 'block');
-  }else{
-    $('.qImage').css('display', 'none');
-  }
-  $('.options:nth-child(1)').text(qList[ques]['Option a (Correct)']);
-  $('.options:nth-child(2)').text(qList[ques]['Option b']);
-  $('.options:nth-child(3)').text(qList[ques]['Option c']);
-  $('.options:nth-child(4)').text(qList[ques]['Option d']);
-  if(ref===0){
-    $('.back').addClass('disabled');
-  }else{
-    $('.back').removeClass('disabled');
-  }
-
-  if(ref===totalQuestions-1){
-    $('.next').addClass('disabled');
-  }else{
-    $('.next').removeClass('disabled');
-  }
-  return ref; //returns the current question number-1. needed since question can be changed from the qList.
+    if (ref === totalQuestions - 1) {
+        $('.next').addClass('disabled');
+    } else {
+        $('.next').removeClass('disabled');
+    }
+    return ref; //returns the current question number-1. needed since question can be changed from the qList.
 }
 
 
